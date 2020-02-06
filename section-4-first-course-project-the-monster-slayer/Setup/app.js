9327a6efdb9dda934912da076c04e9df905e5395
Graham <herdmanctg@outlook.com>
2020-02-07 08:59:39 +1100

    add dialog when attacking

--- section-4-first-course-project-the-monster-slayer/Setup/app.js
+++ section-4-first-course-project-the-monster-slayer/Setup/app.js
@@ -3,24 +3,35 @@ new Vue({
   data: {
     gameStarted: false,
     yourHealth: 100,
-    monsterHealth: 100
+    monsterHealth: 100,
+    gameDialog: [],
+    damage: 0
   },
   methods: {
     startGame() {
       this.gameStarted = !this.gameStarted;
+      this.gameDialog = [];
+      this.yourHealth = 100;
+      this.monsterHealth = 100;
     },
     attack() {
       this.monsterHealth -= 10;
-      monsterAttack();
+      this.monsterAttack();
     },
     monsterAttack() {
       const randomNum = Math.floor(Math.random() * 10);
       if (randomNum === 7) {
         // special attack
-        this.yourHealth -= 30;
+        this.damage = 30;
+        this.yourHealth -= this.damage;
+        this.gameDialog.push(
+          `Special Attack! Monster Hits Player For ${this.damage}`
+        );
       } else {
         // normal attack
-        this.yourHealth -= 10;
+        this.damage = 10;
+        this.yourHealth -= this.damage;
+        this.gameDialog.push(`Monster Hits Player For ${this.damage}`);
       }
     }
   }
