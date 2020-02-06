52c810402e8e1a92f119c0b50f02497716a74fde
Graham <herdmanctg@outlook.com>
2020-02-07 07:31:23 +1100

    add attack method

--- section-4-first-course-project-the-monster-slayer/Setup/app.js
+++ section-4-first-course-project-the-monster-slayer/Setup/app.js
@@ -1,6 +1,27 @@
 new Vue({
   el: "#app",
   data: {
-    message: "Hello Vue"
+    gameStarted: false,
+    yourHealth: 100,
+    monsterHealth: 100
+  },
+  methods: {
+    startGame() {
+      this.gameStarted = !this.gameStarted;
+    },
+    attack() {
+      this.monsterHealth -= 10;
+      monsterAttack();
+    },
+    monsterAttack() {
+      const randomNum = Math.floor(Math.random() * 10);
+      if (randomNum === 7) {
+        // special attack
+        this.yourHealth -= 30;
+      } else {
+        // normal attack
+        this.yourHealth -= 10;
+      }
+    }
   }
 });
