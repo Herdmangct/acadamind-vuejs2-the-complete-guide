41fc54ea3004f0004d89dce29f90fbb10f20ecc7
Graham <herdmanctg@outlook.com>
2020-02-13 07:05:49 +1100

    add healing dialog

--- section-4-first-course-project-the-monster-slayer/Setup/app.js
+++ section-4-first-course-project-the-monster-slayer/Setup/app.js
@@ -31,10 +31,9 @@ new Vue({
     attack() {
       const damage = this.generateRandomNum();
       this.monsterHealth -= damage;
-      this.gameDialog.push(
+      this.gameDialog.unshift(
         this.generateGameDialog(false, "player", "monster", damage)
       );
-
       if (this.isGameOver()) return;
 
       this.monsterAttack();
@@ -42,7 +41,7 @@ new Vue({
     specialAttack() {
       const damage = this.generateRandomNum();
       this.monsterHealth -= damage;
-      this.gameDialog.push(
+      this.gameDialog.unshift(
         this.generateGameDialog(true, "player", "monster", damage)
       );
 
@@ -55,32 +54,39 @@ new Vue({
         // special attack
         const damage = this.generateRandomNum(3);
         this.yourHealth -= damage;
-        this.gameDialog.push(
+        this.gameDialog.unshift(
           this.generateGameDialog(true, "monster", "player", damage)
         );
       } else {
         // normal attack
         const damage = this.generateRandomNum();
         this.yourHealth -= damage;
-        this.gameDialog.push(
+        this.gameDialog.unshift(
           this.generateGameDialog(false, "monster", "player", damage)
         );
       }
       this.isGameOver();
     },
     heal() {
-      if (this.yourHealth < 100) {
-        this.yourHealth += this.generateRandomNum();
-        this.yourHealth = this.yourHealth > 100 ? 100 : this.yourHealth;
-        if (this.generateRandomNum() === 7 && this.monsterHealth < 100) {
-          this.monsterHealth += this.generateRandomNum();
+      const heal = this.generateRandomNum();
+      if (this.yourHealth + heal < 100) {
+        this.yourHealth += heal;
+        // random monster healing
+        if (this.generateRandomNum() === 7 && this.monsterHealth + heal < 100) {
+          this.monsterHealth += heal;
         }
       }
     },
-    generateGameDialog(isSpecial, player1, player2, damage) {
+    generateGameDialog(type, player1, player2, damage) {
       const result = isSpecial
         ? `Special Attack! ${player1} Hits ${player2} For ${damage}`
         : `${player1} Hits ${player2} For ${damage}`;
+      switch (type.toLowerCase) {
+        case "special":
+          return `Special Attack! ${player1} Hits ${player2} For ${damage}`;
+        case "heal":
+          return `${player1} Heals For `;
+      }
       return result.toUpperCase();
     },
     generateRandomNum(specialDamage = 1) {
